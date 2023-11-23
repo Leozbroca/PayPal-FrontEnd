7c3905f10d5a03c101d854f2732b10ef6e29372b
leozbroca <leo.brokka@gmail.com>
2023-11-23 15:54:40 -0300

    First commit

---
+++ paypal-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header/header";
+import Home from "./Page/home";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
---
+++ paypal-frontend/src/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const MainHeader = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 5;
+  width: 100vw;
+  min-height: 120px;
+  max-height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: blue;
+`;
